90fb337fadb4429c451fe68cbb6d579da8d79b9b
elleinad <janjaaan12@gmail.com>
2026-05-16 02:02:59 +0800

    Added seed script for demo data, also ran the seed script to generate 25 products to the table

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+async function main() {
+  const demoUserId = "cf949e5a-1536-4194-8b8f-9cc3932bccd8";
+
+  await prisma.product.createMany({
+    data: Array.from({ length: 25 }).map((_, i) => ({
+      userId: demoUserId,
+      name: `Product ${i + 1}`,
+      price: parseFloat((Math.random() * 90 + 10).toFixed(2)),
+      quantity: Math.floor(Math.random() * 20),
+      lowStockAt: 5,
+      createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+    })),
+  });
+
+  console.log("Seed data created successfully");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
